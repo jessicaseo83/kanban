1250a6141c32242acd1b8707d2e6085525ef1486
Jessica Seo <jessicaseo83@gmail.com>
2020-08-28 18:23:07 -0400

    Implement data state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Navbar';
 import './styles/index.css';
+import initialData from './data/Initialdata';
 
 function App() {
+  const [ data, setData ] = useState(initialData);
+  const columns = data.columnOrder.map(columnId => {
+    const column = data.columns[columnId];
+    const tasks = column.taskIds.map(taskId => data.tasks[taskId])
+
+    return column.title;
+  })
   return (
     <div className="App">
       <Header />
+      {columns}
     </div>
   );
 }
