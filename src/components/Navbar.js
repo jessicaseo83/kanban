1c9380be3a3de1804f24a7ebd02183bc90b5a86c
Eileen Li <eileenlimur@gmail.com>
2020-08-29 02:11:30 -0400

    complete savePdf function in Navbar. Currently won't capture Header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import Button from 'react-bootstrap/Button';
 import html2canvas from 'html2canvas';
 
 export default function Header() {
-  const pdf = function() {
+  const savePdf = function() {
     htm2canvas(document.querySelector('#capture')).then(canvas=> {
       document.body.appendChild(canvas);
     })
@@ -12,9 +12,7 @@ export default function Header() {
 
   return (
     <Navbar bg="light" fixed="top">
-      <Button onClick = {
-        
-      }>
+      <Button onClick = {savePdf}>
         Print PDF
       </Button>
       <Navbar.Brand>Brand text</Navbar.Brand>
