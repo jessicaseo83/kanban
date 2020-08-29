25f92c82531bba50b741c6e4c0af9e785ceaa54f
Eileen Li <eileenlimur@gmail.com>
2020-08-29 02:02:56 -0400

    add html2canvas to deps

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
+import html2canvas from 'html2canvas';
 
 export default function Header() {
   const pdf = function() {
