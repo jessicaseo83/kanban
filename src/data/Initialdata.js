448a95e069e166ed9c76ea5eec4450add5a5867d
Jessica Seo <jessicaseo83@gmail.com>
2020-08-28 23:17:21 -0400

    Modify initial data

--- src/data/Initialdata.js
+++ src/data/Initialdata.js
@@ -3,26 +3,30 @@ const initialData = {
     'task-1': {id: 'task-1', title: 'Take out the garbage', detail: 'Take out the stinky garbage!!!'},
     'task-2': {id: 'task-2', title: 'Go for a walk with my dog', detail: ''},
     'task-3': {id: 'task-3', title: 'Write a song',
-  detail: ''}
+  detail: ''},
+    'task-4': {id: 'task-4', title: 'Drink some wine',
+    detail: 'Drink 1 bottle of chardonnay'},
+    'task-5': {id: 'task-5', title: 'Sleep like a baby',
+    detail: 'Sleep for 10 hours'}
   },
   columns: {
-    'column-1' : {
+    'c1' : {
       id: 'c1',
       title: 'To do',
       taskIds: ['task-1', 'task-2', 'task-3'],
     },
-    'column-2' : {
+    'c2' : {
       id: 'c2',
       title: 'In progress',
-      taskIds: [],
+      taskIds: ['task-4'],
     },
-    'column-3' : {
+    'c3' : {
       id: 'c3',
       title: 'Done',
-      taskIds: [],
+      taskIds: ['task-5'],
     }
   },
-  columnOrder: ['column-1', 'column-2', 'column-3']
+  columnOrder: ['c1', 'c2', 'c3']
 }
 
 export default initialData;
