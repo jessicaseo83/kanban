2c55c7bf1dcac7f7568150eb8162d797ccb7e60b
Eileen Li <eileenlimur@gmail.com>
2020-08-30 14:44:39 -0400

    move contextmenu logic from ContextMenu component into Column.js

--- src/components/ContextMenu.js
+++ src/components/ContextMenu.js
@@ -3,18 +3,7 @@ import React, { useState, useEffect } from 'react';
 export default function ContextMenu(){
   const [state, setState] = useState({visible: false, x: 0, y: 0});
   
-  useEffect(()=>{
-    document.addEventListener('contextmenu', function(event){
-      event.preventDefault();
-      const clickX = event.clientX;
-      const clickY = event.clientY;
-      setState({visible: true, x: clickX, y: clickY});
-    });
-    document.addEventListener('click', function(event) {
-      event.preventDefault();
-      setState({visible: false, x:0, y:0});
-    })
-  }, [])
+
   
   const contextMenuStyle={
     position: 'absolute',
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import Accordion from 'react-bootstrap/Accordion';
 import ContextMenu from './ContextMenu';
@@ -7,6 +7,23 @@ import { BsFillTrashFill } from "react-icons/bs";
 
 
 export default function Task(props) {
+  const [contextMenu, setContextMenu] = useState({visible: false, x: 0, y: 0});
+  const contextMenuStyle={
+    position: 'absolute',
+    top: `${contextMenu.y}px`,
+    left: `${contextMenu.x+5}px`
+  }
+
+  function showContextMenu(event) {
+    event.preventDefault();
+    const clickX = event.clientX;
+    const clickY = event.clientY;
+    setContextMenu({visible: true, x: clickX, y: clickY});
+    document.addEventListener('click', function(event) {
+      event.preventDefault();
+      setContextMenu({visible: false, x:0, y:0});
+    })
+  }
 
   return (
     <Draggable draggableId={props.task.id} index={props.index} >
@@ -17,14 +34,21 @@ export default function Task(props) {
             {...provided.draggableProps}
             {...provided.dragHandleProps}
             ref={provided.innerRef}
+            onContextMenu = {showContextMenu}
             >
+            {contextMenu.visible === true &&
+              <div className='custom-context' id='text' style={contextMenuStyle}>
+                <div className='custom-context-item'>
+                  Move to a different column
+                </div>
+              </div>
+            }
             <Accordion.Toggle as={Card.Header} eventKey="0">{props.task.title}<BsFillTrashFill onClick={props.onDelete}/></Accordion.Toggle>
             <Accordion.Collapse eventKey="0">
               <Card.Body>
                 {props.task.detail}
               </Card.Body>
             </Accordion.Collapse>
-            <ContextMenu/>
           </Card>
         </Accordion>
 
