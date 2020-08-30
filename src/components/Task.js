a2bf5914a54a1ea5b23937c6187d659ac949bd6a
Eileen Li <eileenlimur@gmail.com>
2020-08-30 13:21:01 -0400

    remove oncontextmenu event listener and add contextmenu component to Task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Accordion from 'react-bootstrap/Accordion';
+import ContextMenu from './ContextMenu';
 import { Draggable } from 'react-beautiful-dnd';
 import { BsFillTrashFill } from "react-icons/bs";
 
 
 export default function Task(props) {
-
-  function showContextMenu(event){
-    event.preventDefault();
-    const xPos = event.pageX + "px";
-    const yPos = event.pageY + 'px';
-  }
 
   return (
     <Draggable draggableId={props.task.id} index={props.index} >
@@ -22,7 +17,6 @@ export default function Task(props) {
             {...provided.draggableProps}
             {...provided.dragHandleProps}
             ref={provided.innerRef}
-            onContextMenu={showContextMenu}
             >
             <Accordion.Toggle as={Card.Header} eventKey="0">{props.task.title}<BsFillTrashFill onClick={props.onDelete}/></Accordion.Toggle>
             <Accordion.Collapse eventKey="0">
@@ -30,6 +24,7 @@ export default function Task(props) {
                 {props.task.detail}
               </Card.Body>
             </Accordion.Collapse>
+            <ContextMenu/>
           </Card>
         </Accordion>
 
