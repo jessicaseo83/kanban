f9496626a22bc9c0ea3df5748c692c763b52e5f2
Jessica Seo <jessicaseo83@gmail.com>
2020-08-30 20:31:56 -0400

    Add maximun column quantity feature - by Eileen <3

--- src/App.js
+++ src/App.js
@@ -193,14 +193,16 @@ function App() {
     })
 
   }
+  
 
   return (
     <div className="App" id='pdf'>
+      
       <Header />
       <DragDropContext onDragEnd={onDragEnd}>
         <Droppable droppableId="all-colums" direction="horizontal" type="column" className="column-container">
           {provided => (
-            <div style={{display: "flex", justifyContent: "center"}} 
+            <div className="column-list"
               {...provided.droppableProps}
               ref={provided.innerRef}
             >
@@ -215,7 +217,9 @@ function App() {
           )}
         </Droppable>
       </DragDropContext>
+      
     </div>
+
   );
 }
 
