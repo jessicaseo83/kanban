6fe6aef58729dc66f45fc810a6e34244160b91fd
Eileen Li <eileenlimur@gmail.com>
2020-08-31 01:16:19 -0400

    remove initial data sets

--- src/data/Initialdata.js
+++ src/data/Initialdata.js
@@ -1,31 +1,22 @@
 const initialData = {
-  tasks: {
-    'task-1': {id: 'task-1', title: 'Take out the garbage', detail: 'Take out the stinky garbage!!!'},
-    'task-2': {id: 'task-2', title: 'Go for a walk with my dog', detail: ''},
-    'task-3': {id: 'task-3', title: 'Write a song',
-  detail: ''},
-    'task-4': {id: 'task-4', title: 'Drink some wine',
-    detail: 'Drink 1 bottle of chardonnay'},
-    'task-5': {id: 'task-5', title: 'Sleep like a baby',
-    detail: 'Sleep for 10 hours'}
-  },
+  tasks: {},
   columns: {
     'c1' : {
       id: 'c1',
       title: 'To do',
-      taskIds: ['task-1', 'task-2', 'task-3'],
+      taskIds: [],
       color: '#f6a6ff'
     },
     'c2' : {
       id: 'c2',
       title: 'In progress',
-      taskIds: ['task-4'],
-      color: 'mint'
+      taskIds: [],
+      color: 'mintcream'
     },
     'c3' : {
       id: 'c3',
       title: 'Done',
-      taskIds: ['task-5'],
+      taskIds: [],
       color: '#ffffd1'
     }
   },
