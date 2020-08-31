60bd74c20e9a781a084da8e53c2b6245d7b2df28
Eileen Li <eileenlimur@gmail.com>
2020-08-31 10:07:03 -0400

    add alert with project information

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import html2canvas from 'html2canvas';
 import AlertMsg from './AlertMsg';
@@ -28,8 +29,10 @@ export default function Header(props) {
   }
 
   return (
-    <Navbar className="nav" fixed="top">
-      <Navbar.Brand className="brand-logo">Good Old Kanban</Navbar.Brand>
+    <Navbar className="nav" fixed="top" >
+      <Nav onClick={()=>alert(`This project is a kanban created by Jess and Eileen as part of the Aug. 28 2020 Mintbean JavaScript Bootcamp Olympics hackathon sponsored by FeaturePeek.`)}>
+        <Nav.Link><Navbar.Brand className="brand-logo">Good Old Kanban</Navbar.Brand></Nav.Link>
+      </Nav>
       <AlertMsg show={showAlertMsg} onHide={()=>setShowAlertMsg(false)} deleteComp={handleClearBoard}/>
       <div>
         <Button variant="outline-info" style={{marginRight: '10px'}}onClick = {()=>setShowAlertMsg(true)} className="reset-board-button">
